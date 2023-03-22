7cbb5b6fe14169d27e9fc7a6f7ee0fdaddc42b4d
DovgaNik <nikita.dovgan.05@hotmail.com>
2023-03-22 10:24:36 +0200

    added a menu strip

--- listboxes-oop-22-Mar-2023/Form1.Designer.vb
+++ listboxes-oop-22-Mar-2023/Form1.Designer.vb
@@ -23,6 +23,10 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.ActionsToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.AddAnItemToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MenuStrip1.SuspendLayout()
         Me.SuspendLayout()
         '
         'ListBox1
@@ -30,10 +34,33 @@ Partial Class Form1
         Me.ListBox1.FormattingEnabled = True
         Me.ListBox1.ItemHeight = 20
         Me.ListBox1.Items.AddRange(New Object() {"milk", "bread", "butter", "eggs", "chocholate", "tomatoes", "water"})
-        Me.ListBox1.Location = New System.Drawing.Point(68, 38)
+        Me.ListBox1.Location = New System.Drawing.Point(0, 27)
         Me.ListBox1.Name = "ListBox1"
-        Me.ListBox1.Size = New System.Drawing.Size(212, 324)
+        Me.ListBox1.Size = New System.Drawing.Size(212, 424)
         Me.ListBox1.TabIndex = 0
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.ImageScalingSize = New System.Drawing.Size(20, 20)
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ActionsToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(800, 28)
+        Me.MenuStrip1.TabIndex = 1
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'ActionsToolStripMenuItem
+        '
+        Me.ActionsToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.AddAnItemToolStripMenuItem})
+        Me.ActionsToolStripMenuItem.Name = "ActionsToolStripMenuItem"
+        Me.ActionsToolStripMenuItem.Size = New System.Drawing.Size(72, 24)
+        Me.ActionsToolStripMenuItem.Text = "Actions"
+        '
+        'AddAnItemToolStripMenuItem
+        '
+        Me.AddAnItemToolStripMenuItem.Name = "AddAnItemToolStripMenuItem"
+        Me.AddAnItemToolStripMenuItem.Size = New System.Drawing.Size(224, 26)
+        Me.AddAnItemToolStripMenuItem.Text = "Add an item"
         '
         'Form1
         '
@@ -41,11 +68,19 @@ Partial Class Form1
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
         Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
         Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
     Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents ActionsToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents AddAnItemToolStripMenuItem As ToolStripMenuItem
 End Class
--- listboxes-oop-22-Mar-2023/Form1.vb
+++ listboxes-oop-22-Mar-2023/Form1.vb
@@ -1,3 +1,5 @@
 ﻿Public Class Form1
+    Private Sub AddAnItemToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles AddAnItemToolStripMenuItem.Click
 
+    End Sub
 End Class
