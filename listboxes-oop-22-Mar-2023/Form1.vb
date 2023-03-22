adec37dca0c107d95a35147d01d0d6a3ddbf34fc
DovgaNik <nikita.dovgan.05@hotmail.com>
2023-03-22 10:40:31 +0200

    added check for existing item

--- listboxes-oop-22-Mar-2023/Form1.vb
+++ listboxes-oop-22-Mar-2023/Form1.vb
@@ -2,6 +2,13 @@
     Private Sub AddAnItemToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles AddAnItemToolStripMenuItem.Click
         Dim newItem As String
         newItem = InputBox("Input an item: ", "Add an item")
-        ListBox1.Items.Add(newItem)
+        newItem = newItem.Trim()
+        If newItem <> String.Empty Then
+            If ListBox1.FindStringExact(newItem.Trim()) = -1 Then
+                ListBox1.Items.Add(newItem)
+            Else
+                MessageBox.Show("The item is already added")
+            End If
+        End If
     End Sub
 End Class
