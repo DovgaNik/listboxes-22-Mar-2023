ff95d5e58ba5bc758a98015297dddf465e51619d
DovgaNik <nikita.dovgan.05@hotmail.com>
2023-03-22 10:29:18 +0200

    Added an input box

--- listboxes-oop-22-Mar-2023/Form1.vb
+++ listboxes-oop-22-Mar-2023/Form1.vb
@@ -1,5 +1,7 @@
 ﻿Public Class Form1
     Private Sub AddAnItemToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles AddAnItemToolStripMenuItem.Click
-
+        Dim newItem As String
+        newItem = InputBox("Input an item: ", "Add an item")
+        ListBox1.Items.Add(newItem)
     End Sub
 End Class
